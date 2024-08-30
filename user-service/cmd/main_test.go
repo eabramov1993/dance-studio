6731f00b23a2c0bc98f227cb663f9b5567c2c4a7
Евгений Абрамов <abramov@MacBook-Pro-Evgenij.local>
2024-08-30 08:38:04 +0500

    a

---
+++ user-service/cmd/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/eabramov1993/dance-studio/tree/main/user-service/models"
+	"github.com/eabramov1993/dance-studio/tree/main/user-service/repository"
+)
+
+func TestCreateUser(t *testing.T) {
+	// Настройка подключения к базе данных (замените на ваши данные)
+	db, err := sql.Open("postgres", "postgres://postgres:Aspirine1@localhost:5433/dance_studio?sslmode=disable")
+	if err != nil {
+		t.Fatalf("Ошибка при подключении к базе данных: %v", err)
+	}
+	defer db.Close()
+
+	// Создание тестового пользователя
+	newUser := &models.User{
+		Name:        "Тестовый Пользователь",
+		Email:       "test@example.com",
+		Password:    "testpassword",
+		Role:        "родитель",
+		ContactInfo: "1234567890",
+	}
+
+	// Вызов функции CreateUser
+	err = repository.CreateUser(db, newUser)
+	if err != nil {
+		t.Fatalf("Ошибка при создании пользователя: %v", err)
+	}
+
+	// Проверка, что пользователь был создан
+	var user models.User
+	err = db.QueryRow("SELECT id, name, email, password, role, contact_info FROM users WHERE email = $1", newUser.Email).Scan(
+		&user.ID, &user.Name, &user.Email, &user.Password, &user.Role, &user.ContactInfo)
+	if err != nil {
+		t.Fatalf("Ошибка при получении пользователя из базы данных: %v", err)
+	}
+
+	if user.Name != newUser.Name || user.Email != newUser.Email || user.Role != newUser.Role || user.ContactInfo != newUser.ContactInfo {
+		t.Errorf("Данные созданного пользователя не совпадают с ожидаемыми")
+	}
+}
---
+++ user-service/repository/repository.go
@@ -0,0 +1,28 @@
+package user_service
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/eabramov1993/dance-studio/tree/main/user-service/models"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Функция для создания нового пользователясв юю
+func CreateUser(db *sql.DB, user *models.User) error {
+	// Хэшируем пароль перед сохранением в базу данных
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("ошибка при хэшировании пароля: %v", err)
+	}
+
+	// Выполняем SQL-запрос для вставки данных в таблицу users
+	_, err = db.Exec("INSERT INTO users (name, email, password, role, contact_info) VALUES ($1, $2, $3, $4, $5)",
+		user.Name, user.Email, hashedPassword, user.Role, user.ContactInfo)
+	if err != nil {
+		return fmt.Errorf("ошибка при вставке пользователя в базу данных: %v", err)
+	}
+
+	return nil
+}
